b263fa68564ee8eb865b4a31eb81bbb8db469df8
Dani.Kim <dgkim84@gmail.com>
2011-05-18 17:18:30 +0900

    Fixes #15

--- flume-workflow/src/main/java/com/nexr/rolling/workflow/RollingConstants.java
+++ flume-workflow/src/main/java/com/nexr/rolling/workflow/RollingConstants.java
@@ -14,4 +14,6 @@ public class RollingConstants {
 	public static final String INPUT_PATH = "input.path";
 	public static final String OUTPUT_PATH = "output.path";
 	public static final String RESULT_PATH = "result.path";
+	
+	public static final String TODAY_PATH = "today.path";
 }
